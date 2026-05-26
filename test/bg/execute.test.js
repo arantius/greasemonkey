1f333a4b94fc8e8d2c6e0ef84e95f00c5e556086
Anthony Lieuallen <arantius@gmail.com>
2026-05-26 08:20:04 -0400

    fix test to match new behavior

--- test/bg/execute.test.js
+++ test/bg/execute.test.js
@@ -1,12 +1,16 @@
 'use strict';
 describe('bg/execute', () => {
-  before(() => sinon.stub(UserScriptRegistry, 'scriptsToRunAt'));
+  before(() => {
+    sinon.stub(UserScriptRegistry, 'scriptsToRunAt');
+    // Ensure the promise is resolved
+    UserScriptRegistry._loadUserScripts();
+  });
   after(() => UserScriptRegistry.scriptsToRunAt.restore());
 
-  it('uses tabs.executeScript', () => {
+  it('uses tabs.executeScript', async () => {
     chrome.tabs.executeScript.callsArg(2);
     UserScriptRegistry.scriptsToRunAt.returns([{}]);
-    executeUserscriptOnNavigation({});
+    await executeUserscriptOnNavigation({});
     assert(chrome.tabs.executeScript.calledOnce);
   });
 });
